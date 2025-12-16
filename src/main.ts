86bf9e8c2c6a876cb43fc68dcaed5cf517f3649d
Ovsianyk Oksana <kseniaovsianyk@gmail.com>
2025-12-17 00:22:46 +0200

    feat: fix swagger path to /docs

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,24 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Налаштування префіксу (якщо ви його використовуєте)
   app.setGlobalPrefix('api/v1');
 
-  // Конфігурація Swagger
   const config = new DocumentBuilder()
     .setTitle('Course Management API')
-    .setDescription('The API description for Lab 6 Project Structure')
+    .setDescription('Інтерактивна документація структури проекту Lab 6')
     .setVersion('1.0')
-    .addTag('courses')
     .addTag('users')
+    .addTag('courses')
+    .addTag('lessons')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  // Swagger буде доступний за адресою /api/docs
-  SwaggerModule.setup('api/docs', app, document);
+
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
 
-// Виправляємо помилку floating-promise через .catch()
 bootstrap().catch((err) => {
   console.error(err);
   process.exit(1);
